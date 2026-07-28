091152d1fcffa7c13eb00a33c5661cadcddeebfa
Aritra <aritra.03@proton.me>
2026-07-28 23:32:49 +0530

    Added result page for post payment confirmation

---
+++ assets/scripts/result.js
@@ -0,0 +1,34 @@
+export async function loadDynamicData(ordId) {
+
+    let success;
+    if(ordId === null) return;
+
+    const response = await fetch(`http://localhost:8000/api/order/status/${ordId}`);
+    if(!response.ok){
+        console.log("Fetch err: ", response.message);
+        return;
+    }
+
+    const stat = await response.json();
+    if(response.data === "NOT_PAID") success = false
+    else success = true;
+
+    const defaultState = document.getElementById("result-default");
+    const successState = document.getElementById("result-success");
+    const failureState = document.getElementById("result-failure");
+
+    defaultState.style.display = "none";
+
+    if (success) {
+        successState.style.display = "flex";
+    }
+    else {
+        failureState.style.display = "flex";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+    const params = new URLSearchParams(window.location.search);
+    const ordId = params.get("ord") || null;
+    await loadDynamicData(ordId);
+});
